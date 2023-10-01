633611d8a1fd8c8f675978b22078f5fda814701b
HArt11 <>
2023-10-01 17:54:23 +0000

    secret key removal and debug settings changed

--- hart_botanics/settings.py
+++ hart_botanics/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_ge(149ppcua+j%ihfj2)wx(*$mk()=sgtwr3l5ew+(d(f1&n&'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-hart11-hart-botanics-g552o9dmkf.us2.codeanyapp.com', '8001-hart11-hart-botanics-g552o9dmkf.us2.codeanyapp.com', '8000-hart11-hart-botanics-rkft4yohqn.us2.codeanyapp.com', 'localhost', 'hart-botanics.herokuapp.com']
